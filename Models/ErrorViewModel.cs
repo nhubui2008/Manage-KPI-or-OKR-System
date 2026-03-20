31c8e2df3385dfea6984001a5c1eb274ae42f3b7
quanptatb <quanptatb01758@gmail.com>
2026-03-20 07:21:29 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Manage_KPI_or_OKR_System.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
