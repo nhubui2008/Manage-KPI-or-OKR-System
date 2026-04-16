2139a99c4cb984ac68dd35bed3988234587cb089
quanptatb <quanptatb01758@gmail.com>
2026-04-17 06:25:41 +0700

    feat: implement landing page and configure HomeController as default application entry point

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,10 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        if (User.Identity != null && User.Identity.IsAuthenticated)
+        {
+            return RedirectToAction("Index", "Dashboard");
+        }
         return View();
     }
 
