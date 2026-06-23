ddc2efa262307988ee3e8119be8c673e2b41c249
NhatKaize <nhatvhhtb01605@gmail.com>
2026-06-23 15:55:27 +0700

    Update Kpi/okr tu dong hoa

---
+++ Migrations/20260623084549_AddLinkedOKRIdToWorkProject.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Manage_KPI_or_OKR_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLinkedOKRIdToWorkProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LinkedOKRId",
+                table: "WorkProjects",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinkedOKRId",
+                table: "WorkProjects");
+        }
+    }
+}
